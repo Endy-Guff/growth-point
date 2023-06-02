4d4e4de07ecf9e4df0741adbeed59dfe74b3c652
Andrey <andrey@MacBook-Pro-VanMac.local>
2023-06-02 14:51:38 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -22,16 +22,30 @@ tl.from('.header__top', 2, {
   ease: "power4.out",
   y: -100
 }, '>-4.5')
-tl.from('.header__content', 6, {
+tl.fromTo('.header__content', 6, {
   ease: "power4.out",
   opacity: 0
+},{
+opacity: 1
 }, '>-1')
 tl.from('.img-5', 6, {
   ease: "power4.out",
   opacity: 0
 }, '>-6')
 
-
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('show');
+    }
+  });
+}
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.program__arrow');
+for (let elm of elements) {
+  observer.observe(elm);
+}
 
 
 
@@ -39,30 +53,159 @@ tl.from('.img-5', 6, {
 // const advantagesItem = gsap.utils.toArray('.advantages__item')
 // const questionsFormItem = gsap.utils.toArray('.questions__form-item')
 
-// if(ScrollTrigger.isTouch !== 1){
-//     ScrollSmoother.create({
-//       wrapper: '.scroll-wrapper',
-//       content: '.scroll-content',
-//       smooth: 1.5,
-//       effects: true
-//     })
-  
-//   gsap.fromTo('.img-7',
+const faqItem = gsap.utils.toArray('.faq__item')
+
+if(ScrollTrigger.isTouch !== 1){
+    ScrollSmoother.create({
+      wrapper: '.scroll-wrapper',
+      content: '.scroll-content',
+      smooth: 1.5,
+      effects: true
+    })
+    gsap.fromTo('.about__title',
+  {
+    y: 80
+  },
+  {
+    y: 0,
+    scrollTrigger:{
+      trigger: '.about__title',
+      start: 150,
+      end: 450,
+      scrub: true
+    }
+  })
+
+  gsap.fromTo('.about__text',
+  {
+    opacity: 0
+  },
+  {
+    opacity: 1,
+    scrollTrigger:{
+      trigger: '.about__text',
+      start: 250,
+      end: 550,
+      scrub: true
+    }
+  })
+
+  gsap.fromTo('.program__item',
+  {
+    opacity: 0
+  },
+  {
+    opacity: 1,
+    scrollTrigger:{
+      trigger: '.program__item',
+      start: 550,
+      end: 750,
+      scrub: true
+    }
+  })
+
+  gsap.fromTo('.program__title',
+  {
+    y: 80
+  },
+  {
+    y: 0,
+    scrollTrigger:{
+      trigger: '.program__title',
+      start: 450,
+      end: 750,
+      scrub: true
+    }
+  })
+
+  gsap.fromTo('.rate__title',
+  {
+    y: 80
+  },
+  {
+    y: 0,
+    scrollTrigger:{
+      trigger: '.rate__title',
+      start: 950,
+      end: 1250,
+      scrub: true
+    }
+  })
+
+  gsap.fromTo('.rate__item-title',
+  {
+    opacity: 0
+  },
+  {
+    opacity: 1,
+    scrollTrigger:{
+      trigger: '.rate__item-title',
+      start: 1050,
+      end: 1350,
+      scrub: true
+    }
+  })
+
+  gsap.fromTo('.rate__list',
+  {
+    scale: 0.9,
+    y:25,
+    opacity: 0
+  },
+  {
+    scale: 1,
+    y:0,
+    opacity: 1,
+    scrollTrigger:{
+      trigger: '.rate__list',
+      start: 1350,
+      end: 1550,
+      scrub: true
+    }
+  })
+
+  gsap.fromTo('.faq__title',
+  {
+    y: 80
+  },
+  {
+    y: 0,
+    scrollTrigger:{
+      trigger: '.faq__title',
+      start: 1650,
+      end: 1850,
+      scrub: true
+    }
+  })
+
+  faqItem.forEach(item=>{
+    gsap.fromTo(item,
+  {
+    scale: 0.9,
+    y:25,
+    opacity: 0
+  },
+  {
+    scale: 1,
+    y:0,
+    opacity: 1,
+    scrollTrigger:{
+      trigger: item,
+      end: 2000,
+      scrub: true
+    }
+  })
+  })
+}
+//   gsap.fromTo('.program__arrow',
 //   {
-//     rotate: 0,
-//     x: 0,
-//     y: 0
+//     drawSVG: 0
 //   },
 //   {
-//     rotate: 40,
-//     x: -120,
-//     y: 40,
+//     drawSVG: '100%',
 //     scrollTrigger:{
-//       trigger: '.img-7',
-//       start: 50,
-//       end: 500,
+//       trigger: '.program__arrow',
 //       scrub: true
 //     }
 //   })
 
-// }
